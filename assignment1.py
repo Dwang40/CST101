9788fd1d14cdb1ea0d444d86fc1e837c7e61c392
Dwang40 <124080285+Dwang40@users.noreply.github.com>
2025-02-24 23:44:28 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,5 @@
 name = input("Enter your name: ")
-hours_worked = int(input("Enter the total number of hours you work weekly: "))
+hours_worked = int(input("Enter the total number of hours you work bi-weekly: "))
 pay_rate = int(input("Enter your hourly pay rate: "))
 week = hours_worked * pay_rate
-print(name, ", you make ", week, " dollars every week.", sep = "")
+print(name, ", you make ", week, " dollars every 2 weeks.", sep = "")
